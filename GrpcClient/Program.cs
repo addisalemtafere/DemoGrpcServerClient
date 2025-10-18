f7093368e3aafdb89d5f63a595611a6f283f8ada
Addisalem <addisalem12@gmail.com>
2025-10-18 14:05:50 +0300

    Simple grpc sever and client added

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcClient;
+
+Console.WriteLine("Hello, World!");
+using var channel = GrpcChannel.ForAddress("http://localhost:5081");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GrpcClient" });
+Console.WriteLine("Greeting: " + reply.Message);
